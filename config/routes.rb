d6c7dfc1d2f1043de17711ff082d77952a131e6f
Sam Serrano <samuel.j.serrano@gmail.com>
2026-07-16 19:47:04 -0700

    Implement public landing route
    Refs #13
    
    Part of #8

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   get "status" => "status#show"
+  root "landing#show"
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end

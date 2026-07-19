0630002deb05c0018f8921fb78d1b5f03c7013f4
fairybladderwort <owner@segfaultsoftware.com>
2026-07-18 23:55:32 -0700

    Implement RPI task #16: Implement Google login, remembered sessions, and logout (#30)
    * Implement Google login sessions and logout
    
    Refs #16
    
    Part of #9
    
    * Clarify Google OAuth login helpers
    
    Refs #16
    
    Part of #9
    
    * Update sanitizer dependencies
    
    Refs #16
    
    Part of #9
    
    ---------
    
    Co-authored-by: Sam Serrano <samuel.j.serrano@gmail.com>

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,11 @@
+class SessionsController < ApplicationController
+  include Devise::Controllers::Rememberable
+
+  before_action :authenticate_user!
+
+  def destroy
+    forget_me(current_user)
+    sign_out(:user)
+    redirect_to root_path, status: :see_other
+  end
+end

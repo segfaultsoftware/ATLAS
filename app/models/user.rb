6266847bfca4dfea63201fbcfb54391dd2e903fb
fairybladderwort <owner@segfaultsoftware.com>
2026-07-20 20:42:17 -0700

    Implement Google sign-in profile creation (#33)
    Refs #19
    
    Part of #10
    
    Co-authored-by: Sam Serrano <samuel.j.serrano@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,19 @@ class User < ApplicationRecord
   validates :uid, uniqueness: { scope: :provider }
 
   def self.find_or_create_from_google_oauth!(auth)
-    find_or_initialize_by(provider: auth.provider, uid: auth.uid).tap do |user|
-      user.email = auth.info.email
-      user.name = auth.info.name
-      user.save!
+    transaction do
+      find_or_initialize_by(provider: auth.provider, uid: auth.uid).tap do |user|
+        user.email = auth.info.email
+        user.name = auth.info.name
+        user.save!
+        user.ensure_profile!(preferred_name: auth.info.name)
+      end
+    end
+  end
+
+  def ensure_profile!(preferred_name:)
+    with_lock do
+      profile || create_profile!(preferred_name: preferred_name.presence)
     end
   end
 end
